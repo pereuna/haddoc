3f500c2788d7e4182de629b97024b13db8fa595d
peeru <petteri.reunamo@protonmail.com>
2023-03-17 14:18:47 +0200

    ny 64

---
+++ preslib-01/color_names.c
@@ -0,0 +1,77 @@
+#include <X11/Xcms.h>
+/*Code gen: color themes for different situations*/
+
+enum color_theme_names
+{
+  DAY_BRIGHT,
+  DAY_BLACKBACK,
+  DAY_WHITEBACK,
+  DUSK,
+  NIGHT,
+};
+
+/*Code gen: color names*/
+
+XcmsColor NODTA[5];
+XcmsColor CURSR[5];
+XcmsColor CHBLK[5];
+XcmsColor CHGRD[5];
+XcmsColor CHGRF[5];
+XcmsColor CHRED[5];
+XcmsColor CHGRN[5];
+XcmsColor CHYLW[5];
+XcmsColor CHMGD[5];
+XcmsColor CHMGF[5];
+XcmsColor CHBRN[5];
+XcmsColor CHWHT[5];
+XcmsColor SCLBR[5];
+XcmsColor CHCOR[5];
+XcmsColor LITRD[5];
+XcmsColor LITGN[5];
+XcmsColor LITYW[5];
+XcmsColor ISDNG[5];
+XcmsColor DNGHL[5];
+XcmsColor TRFCD[5];
+XcmsColor TRFCF[5];
+XcmsColor LANDA[5];
+XcmsColor LANDF[5];
+XcmsColor CSTLN[5];
+XcmsColor SNDG1[5];
+XcmsColor SNDG2[5];
+XcmsColor DEPSC[5];
+XcmsColor DEPCN[5];
+XcmsColor DEPDW[5];
+XcmsColor DEPMD[5];
+XcmsColor DEPMS[5];
+XcmsColor DEPVS[5];
+XcmsColor DEPIT[5];
+XcmsColor RADHI[5];
+XcmsColor RADLO[5];
+XcmsColor ARPAT[5];
+XcmsColor NINFO[5];
+XcmsColor RESBL[5];
+XcmsColor ADINF[5];
+XcmsColor RESGR[5];
+XcmsColor SHIPS[5];
+XcmsColor PSTRK[5];
+XcmsColor SYTRK[5];
+XcmsColor PLRTE[5];
+XcmsColor APLRT[5];
+XcmsColor UINFD[5];
+XcmsColor UINFF[5];
+XcmsColor UIBCK[5];
+XcmsColor UIAFD[5];
+XcmsColor UINFR[5];
+XcmsColor UINFG[5];
+XcmsColor UINFO[5];
+XcmsColor UINFB[5];
+XcmsColor UINFM[5];
+XcmsColor UIBDR[5];
+XcmsColor UIAFF[5];
+XcmsColor OUTLW[5];
+XcmsColor OUTLL[5];
+XcmsColor RES01[5];
+XcmsColor RES02[5];
+XcmsColor RES03[5];
+XcmsColor BKAJ1[5];
+XcmsColor BKAJ2[5];
